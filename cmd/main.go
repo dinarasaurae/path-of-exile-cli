6189480594f40c1209fba4a27a9c3d64ad035293
dinarasaurae <khdinova@gmail.com>
2026-05-16 18:03:19 +0300

    added config loading, it reads task config JSON, validate basic values, converts into settings that easier for simulator to use, started writing a skeleton of run func in main - config reading and settings loader

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,36 @@
 package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/dinarasaurae/path-of-exile-cli/internal"
+)
+
+func run(args []string, stdout io.Writer, stderr io.Writer) (err error) {
+	flags := flag.NewFlagSet("path-of-exile-cli", flag.ContinueOnError)
+	flags.SetOutput(stderr)
+
+	configPath := flags.String("config", "config.json", "path to config file")
+
+	if err := flags.Parse(args); err != nil {
+		return err
+	}
+	configFile, err := os.Open(*configPath)
+	if err != nil {
+		return fmt.Errorf("open config: %w", err)
+	}
+	defer func() {
+		if closeErr := configFile.Close(); err == nil && closeErr != nil {
+			err = fmt.Errorf("close config: %w", closeErr)
+		}
+	}()
+
+	settings, err := internal.LoadSettings(configFile)
+	if err != nil {
+		return fmt.Errorf("load config: %w", err)
+	}
+	return nil
+}
---
+++ internal/config.go
@@ -0,0 +1,52 @@
+package internal
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"time"
+)
+
+type Config struct {
+	Floors   int
+	Monsters int
+	OpenAt   string
+	Duration int
+}
+
+type Settings struct {
+	Floors        int
+	Monsters      int
+	OpenAt        time.Duration
+	CloseAt       time.Duration
+	OrdinaryCount int
+}
+
+func LoadSettings(reader io.Reader) (Settings, error) {
+	var config Config
+	if err := json.NewDecoder(reader).Decode(&config); err != nil {
+		return Settings{}, err
+	}
+
+	return NewSettings(config)
+}
+
+func NewSettings(config Config) (Settings, error) {
+	if config.Floors < 1 {
+		return Settings{}, fmt.Errorf("floors must be positive")
+	}
+
+	if config.Monsters < 0 {
+		return Settings{}, fmt.Errorf("monsters cannot be negative")
+	}
+
+	if config.Duration <= 0 {
+		return Settings{}, fmt.Errorf("duration must be positive")
+	}
+
+	return Settings{
+		Floors:        config.Floors,
+		Monsters:      config.Monsters,
+		OrdinaryCount: config.Floors - 1,
+	}, nil
+}
